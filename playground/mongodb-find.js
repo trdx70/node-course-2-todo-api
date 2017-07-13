29b6e459e68f42acc9929e2b870f4c84aa35ef68
Sonny Trinidad <strinidad70@gmail.com>
2017-07-12 17:01:34 -0700

    Initial Commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,34 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+
+  console.log('Connected to MongoDB server');
+
+ // db.collection('Todos').find({
+ //   _id: new ObjectID("5966ad5fda1886d7282c23ef")
+ // }).toArray().then((docs)=>{
+ //   console.log('Todos');
+ //   console.log(JSON.stringify(docs,undefined, 2));
+ // },(err)=>{
+ //   console.log('Unable to fetch todos', err);
+ // });
+
+ // db.collection('Todos').find().count().then((count)=>{
+ //   console.log(`Todos count, ${count}`);
+ //   console.log(JSON.stringify(docs,undefined, 2));
+ // },(err)=>{
+ //   console.log('Unable to fetch todos', err);
+ // });
+
+ db.collection('Users').find({
+   name: "Sonny Trinidad"
+ }).toArray().then((docs)=>{
+   console.log(JSON.stringify(docs,undefined,2));
+ })
+
+  db.close();
+});
